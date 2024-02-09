752006d3b462f2a0876ba0fa60ed8435f594079b
Mik <mik23@protonmail.ch>
2024-02-09 14:01:44 +0530

    added tests and ticket screen

---
+++ __tests__/AirlineFilter.test.tsx
@@ -0,0 +1,26 @@
+// test to check if airlines are rendered correctly
+
+// Import your necessary testing utilities and the component under test
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import AirlineFilter from '@/Components/AirlineFilter'; // replace with actual path to AirlineFilter
+
+describe('AirlineFilter', () => {
+  it('Renders Jet Spice', () => {
+    const {getByText, getByTestId} = render(
+      <AirlineFilter airlineCode="AB" airlineName="Jet Spice" />,
+    );
+
+    // Check if the airline name is present
+    expect(getByText('Jet Spice')).toBeTruthy();
+  });
+
+  it('Renders Air India', () => {
+    const {getByText, getByTestId} = render(
+      <AirlineFilter airlineCode="CD" airlineName="Air India" />,
+    );
+
+    // Check if the airline name is present
+    expect(getByText('Air India')).toBeTruthy();
+  });
+});
